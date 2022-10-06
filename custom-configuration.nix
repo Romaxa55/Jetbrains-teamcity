b106fde10753752c87983403623cb1a735cf1ef8
Шамагин Роман Олегович <ROShamagin@sberbank.ru>
2022-10-07 04:27:54 +0500

    test

--- custom-configuration.nix
+++ custom-configuration.nix
@@ -1,30 +1,7 @@
 { config, options, lib, pkgs, ... }:
 
 {
-    imports =
-    [ # Include the results of the hardware scan.
-    ./configuration.nix
-    ./hardware-configuration.nix
-    ./vagrant-hostname.nix
-    ./vagrant-network.nix
-    ./vagrant.nix
-    ];
-#
-#    virtualisation.docker.enable = true;
-#    users.users.vagrant.extraGroups = [ "docker" ];
 
-    environment.systemPackages = [
-    pkgs.arion
-
-     # Do install the docker CLI to talk to podman.
-     # Not needed when virtualisation.docker.enable = true;
-    pkgs.docker-client
-  ];
-  virtualisation.docker.enable = false;
-  virtualisation.podman.enable = true;
-  virtualisation.podman.dockerSocket.enable = true;
-  virtualisation.podman.defaultNetwork.dnsname.enable = true;
-  users.extraUsers.vagrant.extraGroups = ["podman"];
 
 #    containerImage = pkgs.dockerTools.buildLayeredImage
 #      {

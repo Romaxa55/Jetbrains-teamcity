d03b6d994d24cecb033b42f251d6204634a12bbd
Шамагин Роман Олегович <ROShamagin@sberbank.ru>
2022-10-08 23:54:45 +0500

    Uso one instance

--- cloud-terraform/main.tf
+++ cloud-terraform/main.tf
@@ -19,8 +19,8 @@ resource "yandex_compute_instance" "gateway-a" {
   }
 
   resources {
-    cores         = 4
-    memory        = 8
+    cores         = 2
+    memory        = 4
     core_fraction = 100
   }
 
@@ -36,33 +36,3 @@ resource "yandex_compute_instance" "gateway-a" {
     subnet_id = yandex_vpc_subnet.qa-subnet-a.id
   }
 }
-//
-//resource "yandex_compute_instance" "gateway-b" {
-//  name        = "gateway-centos-7-b"
-//  description = "Gateway instance"
-//  platform_id = "standard-v1"
-//  zone        = "ru-central1-b"
-//
-//  labels = {
-//    enviroment = "qa"
-//    type       = "gateway"
-//  }
-//
-//  resources {
-//    cores         = 2
-//    memory        = 4
-//    core_fraction = 100
-//  }
-//
-//  boot_disk {
-//    auto_delete = true
-//    initialize_params {
-//      image_id = "fd83869rbingor0in0ui"
-//      type     = "network-ssd"
-//    }
-//  }
-//
-//  network_interface {
-//    subnet_id = yandex_vpc_subnet.qa-subnet-b.id
-//  }
-//}

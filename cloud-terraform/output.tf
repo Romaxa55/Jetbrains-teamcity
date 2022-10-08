cffae095dcd2e52e689bc318f06327f6dcce71ae
Шамагин Роман Олегович <ROShamagin@sberbank.ru>
2022-10-08 23:54:45 +0500

    Uso one instance

--- cloud-terraform/output.tf
+++ cloud-terraform/output.tf
@@ -6,10 +6,3 @@ output "external_ip_address_gateway_a" {
   value = yandex_compute_instance.gateway-a.network_interface.0.nat_ip_address
 }
 
-output "internal_ip_address_gateway_b" {
-  value = yandex_compute_instance.gateway-b.network_interface.0.ip_address
-}
-
-output "external_ip_address_gateway_b" {
-  value = yandex_compute_instance.gateway-b.network_interface.0.nat_ip_address
-}

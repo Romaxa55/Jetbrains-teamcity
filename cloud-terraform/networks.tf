74bd61b9c0315b98760792a25cf564390dbfc923
Шамагин Роман Олегович <ROShamagin@sberbank.ru>
2022-10-08 23:51:43 +0500

    Add terraform yandex cloud

--- cloud-terraform/networks.tf
+++ cloud-terraform/networks.tf
@@ -1,5 +1,5 @@
 resource "yandex_vpc_network" "qa-net" {
-  name = "qa-network"
+  name        = "qa-network"
   description = "QA network"
   labels = {
     enviroment = "qa"
@@ -10,19 +10,19 @@ resource "yandex_vpc_subnet" "qa-subnet-a" {
   name           = "qa-subnet-a"
   v4_cidr_blocks = ["10.0.0.0/16"]
   zone           = "ru-central1-a"
-  network_id     = "${yandex_vpc_network.qa-net.id}"
+  network_id     = yandex_vpc_network.qa-net.id
 }
 
 resource "yandex_vpc_subnet" "qa-subnet-b" {
   name           = "qa-subnet-b"
   v4_cidr_blocks = ["10.1.0.0/16"]
   zone           = "ru-central1-b"
-  network_id     = "${yandex_vpc_network.qa-net.id}"
+  network_id     = yandex_vpc_network.qa-net.id
 }
 
 resource "yandex_vpc_subnet" "qa-subnet-c" {
   name           = "qa-subnet-c"
   v4_cidr_blocks = ["10.2.0.0/16"]
   zone           = "ru-central1-c"
-  network_id     = "${yandex_vpc_network.qa-net.id}"
+  network_id     = yandex_vpc_network.qa-net.id
 }
